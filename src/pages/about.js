b0cf2eb3cbd746f5f8aeae0f5791b97315978205
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-03-02 18:35:24 -0800

    Add pages and routes

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+        </div>
+    )
+}
