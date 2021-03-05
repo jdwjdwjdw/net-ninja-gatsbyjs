b95b6c39c0a062f9996922e4bd888288e55b811f
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-03-04 18:22:45 -0800

    Add Layout component to wrap page components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
-        </div>
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut non quia sequi voluptatibus veritatis dolorem debitis pariatur est earum saepe iure ipsum culpa, cupiditate repellat harum quibusdam a id accusantium.</p>
+            </div>
+        </Layout>
     )
 }
