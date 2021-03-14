a921ea5012e1f93da668162ad58ed56759e093ab
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-03-13 22:51:53 -0800

    Add image thumbnails and featured image for the projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import styles from '../../styles/projects.module.css'
+import Img from 'gatsby-image'
 
 export default function Projects({ data }) {
     console.log(data)
@@ -17,6 +18,7 @@ export default function Projects({ data }) {
                     {projects.map(project => (
                         <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                                 <h3>{ project.frontmatter.title }</h3>
                                 <p>{ project.frontmatter.stack }</p>
                             </div>
@@ -31,21 +33,28 @@ export default function Projects({ data }) {
 
 // export page query
 export const query = graphql`
-    query ProjectsPage {
-        projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-            nodes {
-            frontmatter {
-                slug
-                stack
-                title
+query ProjectsPage {
+  projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        slug
+        stack
+        title
+        thumb {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
             }
-            id
-            }
+          }
         }
-        contact: site {
-            siteMetadata {
-                contact
-            }
-        }
+      }
+      id
     }
+  }
+  contact: site {
+    siteMetadata {
+      contact
+    }
+  }
+}
 `;
