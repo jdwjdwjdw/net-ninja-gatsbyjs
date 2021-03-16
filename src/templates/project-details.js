5a3c272a1946b99f3cbd447a819a3b1e86e34253
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-03-15 17:19:39 -0700

    Add query for detailed project pages based on slug query variable, dynamically display data from query on pages

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,43 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
-// import { GatsbyImage } from "gatsby-plugin-image";
+import Img from "gatsby-image";
 import styles from '../styles/project-details.module.css'
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+    const { html } = data.markdownRemark
+    const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
     return (
         <Layout>
             <div className={styles.details}>
-                <h2>title</h2>
-                <h3>stack</h3>
+                <h2>{title}</h2>
+                <h3>{stack}</h3>
                 <div className={styles.featured}>
-                    {/* <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} /> */}
+                    <Img fluid={featuredImg.childImageSharp.fluid} />
                 </div>
-                {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query ProjectDetails($slug: String) {
+        markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+        html
+        frontmatter {
+            stack
+            title
+            featuredImg {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+            }
+        }
+        }
+    }
+`
+  
